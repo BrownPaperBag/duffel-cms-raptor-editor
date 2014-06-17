d03b42008be70ae6ceee2981673d570230cdd1e6
Michael Robinson <mike@pagesofinterest.net>
2014-06-17 12:14:40 +1200

    Updated initialisation with proper docking settings

--- private/javascript/raptor-initialisation.js
+++ private/javascript/raptor-initialisation.js
@@ -1,16 +1,16 @@
-raptor(function($) {
+(function() {
   $('.cms-block').raptor({
     plugins: {
       dock: {
         docked: true,
         dockToScreen: true,
-        dockUnder: '.duffel-visor-spacer'
+        under: '.duffel-visor',
       },
       save: {
         plugin: 'saveRest'
       },
       saveRest: {
-        url: '/cms/admin/content/',
+        url: '/duffel-cms/admin/content/',
         data: function(html) {
           return {
             name: this.raptor.getElement().data('name'),
@@ -22,4 +22,4 @@ raptor(function($) {
       }
     }
   });
-});
+})();
